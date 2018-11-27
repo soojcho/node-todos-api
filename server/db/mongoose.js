60dc77b492941ec9a72c351acf006e66bcbcf85e
Soo J. Cho <beantownbuilder@gmail.com>
2018-11-26 23:50:00 -0500

    added heroku url in comment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+//https://cryptic-refuge-26436.herokuapp.com/
 
 module.exports={
   mongoose
